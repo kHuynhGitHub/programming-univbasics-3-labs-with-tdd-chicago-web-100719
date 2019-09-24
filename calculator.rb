3407e105efd75f6c9ac7a6d2ee8cbc8ef801c76f
Kimberly Huynh <huynh.h.kimberly@gmail.com>
2019-09-24 00:54:03 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,6 @@
 first_number = int
+second_number = 
+sum = first_number + second_number
+difference = first_number - second_number
+product = first_number * second_number
+quotient = first_number / second_number
